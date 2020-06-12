2c6af5cf271b93146eaa999e4e8c61a223312ba0
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-06-12 07:50:31 -0300

    Well at least half of the interface is done properly now

--- src/main/kotlin/io/github/cafeteriaguild/advweaponry/gui/TableController.kt
+++ src/main/kotlin/io/github/cafeteriaguild/advweaponry/gui/TableController.kt
@@ -5,7 +5,6 @@ import io.github.cafeteriaguild.advweaponry.gui.widgets.AWWGridPanel
 import io.github.cafeteriaguild.advweaponry.gui.widgets.WSelectableItemSlot
 import io.github.cafeteriaguild.advweaponry.identifier
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription
-import io.github.cottonmc.cotton.gui.widget.WGridPanel
 import io.github.cottonmc.cotton.gui.widget.WItemSlot
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.screen.ScreenHandlerContext
@@ -30,22 +29,12 @@ class TableController(syncId: Int, playerInventory: PlayerInventory, screenHandl
             x++
         }
         // MODIFIERS SLOTS
-        x = 4
-        y = 0
-        (12 until 20).forEach { slot ->
-            val itemWidget = WItemSlot.of(blockInventory, slot)
-            if (x >= 6) {
-                y++
-                x = 4
-            }
-            panel.add(itemWidget, x, y)
-            x++
-        }
+        val modifierWidgets = WItemSlot.of(blockInventory, 12, 2, 4)
+        panel.add(modifierWidgets, 4, 0)
         // MATERIALS SLOTS
-        (20 until 29).forEach { slot ->
-            val itemWidget = WItemSlot.of(blockInventory, slot)
-            panel.add(itemWidget, slot - 20, 5)
-        }
+        val materialsWidgets = WItemSlot.of(blockInventory, 20, 9, 1)
+        
+        panel.add(materialsWidgets, 0, 5)
         val inputItemWidget = WItemSlot.of(blockInventory, 29)
         panel.add(inputItemWidget, 7.0, 0.5)
         val outputItemWidget = WItemSlot.outputOf(blockInventory, 30)

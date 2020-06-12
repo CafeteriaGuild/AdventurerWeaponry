ee13185e8418d78404ee7e309dc7bb47b4827676
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-06-12 13:47:34 -0300

    Added support for Storm Arrow modifiers on Bows

---
+++ src/main/java/io/github/cafeteriaguild/advweaponry/mixin/MixinBowItem.java
@@ -0,0 +1,110 @@
+package io.github.cafeteriaguild.advweaponry.mixin;
+
+import io.github.cafeteriaguild.advweaponry.items.modifiers.Modifiers;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
+
+@Mixin(BowItem.class)
+public abstract class MixinBowItem extends Item {
+    public MixinBowItem(Settings settings) {
+        super(settings);
+    }
+
+    @Shadow
+    public abstract int getMaxUseTime(ItemStack stack);
+
+    @Shadow
+    public static float getPullProgress(int useTicks) {
+        return 0;
+    }
+
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity) {
+            PlayerEntity playerEntity = (PlayerEntity) user;
+            boolean bl = playerEntity.abilities.creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0;
+            ItemStack itemStack = playerEntity.getArrowType(stack);
+            if (!itemStack.isEmpty() || bl) {
+                if (itemStack.isEmpty()) {
+                    itemStack = new ItemStack(Items.ARROW);
+                }
+
+                int i = this.getMaxUseTime(stack) - remainingUseTicks;
+                float f = getPullProgress(i);
+                if (stack.getTag() != null && stack.getTag().contains("AWModifiers")) {
+                    ListTag listTag = stack.getTag().getList("AWModifiers", 10);
+                    for (Tag tag : listTag) {
+                        CompoundTag compoundTag = (CompoundTag) tag;
+                        String id = compoundTag.getString("Id");
+                        if (id.equals(Modifiers.INSTANCE.getDRAW_TIME_REDUCER().getIdentifier().toString())) {
+                            int lvl = compoundTag.getInt("Lvl");
+                            f += lvl / 10f;
+                        }
+                    }
+                }
+                f = Math.min(f, 1.0f);
+                if ((double) f >= 0.1D) {
+                    boolean bl2 = bl && itemStack.getItem() == Items.ARROW;
+                    if (!world.isClient) {
+                        ArrowItem arrowItem = (ArrowItem) (itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
+                        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
+                        persistentProjectileEntity.setProperties(playerEntity, playerEntity.pitch, playerEntity.yaw, 0.0F, f * 3.0F, 1.0F);
+                        if (f == 1.0F) {
+                            persistentProjectileEntity.setCritical(true);
+                        }
+
+                        int j = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
+                        if (j > 0) {
+                            persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double) j * 0.5D + 0.5D);
+                        }
+
+                        int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
+                        if (k > 0) {
+                            persistentProjectileEntity.setPunch(k);
+                        }
+
+                        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
+                            persistentProjectileEntity.setOnFireFor(100);
+                        }
+
+                        stack.damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(playerEntity.getActiveHand());
+                        });
+                        if (bl2 || playerEntity.abilities.creativeMode && (itemStack.getItem() == Items.SPECTRAL_ARROW || itemStack.getItem() == Items.TIPPED_ARROW)) {
+                            persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                        }
+
+                        world.spawnEntity(persistentProjectileEntity);
+                    }
+
+                    world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (RANDOM.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!bl2 && !playerEntity.abilities.creativeMode) {
+                        itemStack.decrement(1);
+                        if (itemStack.isEmpty()) {
+                            playerEntity.inventory.removeOne(itemStack);
+                        }
+                    }
+
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                }
+            }
+        }
+    }
+}

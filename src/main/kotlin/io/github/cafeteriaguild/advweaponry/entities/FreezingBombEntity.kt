dea3c20c529c06f892b511599b973c6e4efd190e
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-06-12 17:55:17 -0300

    Added Freezing Bomb effect on collision

--- src/main/kotlin/io/github/cafeteriaguild/advweaponry/entities/FreezingBombEntity.kt
+++ src/main/kotlin/io/github/cafeteriaguild/advweaponry/entities/FreezingBombEntity.kt
@@ -4,9 +4,13 @@ import io.github.cafeteriaguild.advweaponry.AWEntities
 import io.github.cafeteriaguild.advweaponry.AWItems
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.block.Blocks
+import net.minecraft.block.SnowBlock
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.mob.BlazeEntity
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity
 import net.minecraft.item.Item
@@ -15,6 +19,8 @@ import net.minecraft.particle.ParticleEffect
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
 import net.minecraft.world.World
 
 class FreezingBombEntity : ThrownItemEntity {
@@ -50,6 +56,24 @@ class FreezingBombEntity : ThrownItemEntity {
     override fun onCollision(hitResult: HitResult?) {
         super.onCollision(hitResult)
         if (!world.isClient) {
+            val hitPos = hitResult!!.pos
+            val centerPos = BlockPos(hitPos.x.toInt(), hitPos.y.toInt(), hitPos.z.toInt())
+            val box = Box(centerPos).expand(2.0)
+            for (x in box.minX.toInt() until box.maxX.toInt())
+                for (y in box.minY.toInt() until box.maxY.toInt())
+                    for (z in box.minZ.toInt() until box.maxZ.toInt()) {
+                        val pos = BlockPos(x, y, z)
+                        val snowLayer = Blocks.SNOW.defaultState
+                        if (world.isAir(pos) && snowLayer.canPlaceAt(world, pos))
+                            world.setBlockState(pos, snowLayer.with(SnowBlock.LAYERS, SnowBlock.LAYERS.values.random()))
+                        else if (world.isWater(pos))
+                            world.setBlockState(pos, Blocks.ICE.defaultState)
+                    }
+            world.getEntities(LivingEntity::class.java, box) { !it.isSpectator }.forEach { entity ->
+                entity.addStatusEffect(StatusEffectInstance(
+                    StatusEffects.SLOWNESS, 200, 3
+                ))
+            }
             world.sendEntityStatus(this, 3.toByte())
             this.remove()
         }

dc71cb80a5c46288fb527ff16f8b672b9afcf09c
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-06-12 13:52:30 -0300

    u n u s e d imports, my bad

--- src/main/java/io/github/cafeteriaguild/advweaponry/mixin/MixinBowItem.java
+++ src/main/java/io/github/cafeteriaguild/advweaponry/mixin/MixinBowItem.java
@@ -16,11 +16,6 @@ import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Consumer;
 
 @Mixin(BowItem.class)
 public abstract class MixinBowItem extends Item {

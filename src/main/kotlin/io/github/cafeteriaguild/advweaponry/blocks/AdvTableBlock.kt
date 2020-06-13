20d671ab9c8ac82ca771d44838953c27a54e9218
jonatsp <jonatsp10@gmail.com>
2020-06-13 00:30:34 -0300

    Add model files

--- src/main/kotlin/io/github/cafeteriaguild/advweaponry/blocks/AdvTableBlock.kt
+++ src/main/kotlin/io/github/cafeteriaguild/advweaponry/blocks/AdvTableBlock.kt
@@ -8,14 +8,27 @@ import net.minecraft.block.BlockState
 import net.minecraft.block.InventoryProvider
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.inventory.SidedInventory
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.DirectionProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.World
 import net.minecraft.world.WorldAccess
 
 class AdvTableBlock(settings: Settings) : Block(settings), InventoryProvider {
+    companion object {
+        val FACING: DirectionProperty = Properties.FACING
+    }
+
+    init {
+        defaultState = stateManager.defaultState.with(FACING, Direction.NORTH)
+    }
+
     override fun getInventory(state: BlockState?, world: WorldAccess?, pos: BlockPos?): SidedInventory =
         AWInventory(31) { _, _ -> true }
 
@@ -33,5 +46,13 @@ class AdvTableBlock(settings: Settings) : Block(settings), InventoryProvider {
             }
         }
         return ActionResult.SUCCESS
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return defaultState.with(FACING, ctx.playerFacing.opposite)
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        builder.add(FACING)
     }
 }
